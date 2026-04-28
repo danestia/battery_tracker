ea7aa1e6b36baf791d594a69917ede6aa53ad1b3
Dan Fleming <dan.fleming@estia.fr>
2026-04-28 13:31:50 +0200

    rebuilding venv

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/dan/Documents/code/github_project/plantform/.venv
+set -gx VIRTUAL_ENV /home/dan/Documents/code/github_project/battery_tracker/.venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
